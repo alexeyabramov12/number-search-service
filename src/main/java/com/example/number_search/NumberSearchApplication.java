5c31874c705665029b2176b5968424c9dfab6367
Абрамов Алексей <a.abramov@ylab.team>
2025-02-05 10:49:38 +0500

    initial commit

---
+++ src/main/java/com/example/number_search/NumberSearchApplication.java
@@ -0,0 +1,13 @@
+package com.example.number_search;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NumberSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NumberSearchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/number_search/NumberSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.number_search;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NumberSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
